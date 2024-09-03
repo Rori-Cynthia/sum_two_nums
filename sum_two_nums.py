0ca8a7b0d809f34ec451e6f4d79f3acf870c28cd
Cynthia Rojas Rivera <rori.cynthia@gmail.com>
2024-09-02 20:06:44 -0400

    Carga código completo programa sum_two_numbers

---
+++ sum_two_nums.py
@@ -0,0 +1,14 @@
+#Define función que suma dos números
+def sum_two_nums():
+    #Define dos variables que guardan los dos números a sumar
+    first_number = 7
+    second_number = 73
+    #Define variable que guarda calculo de suma
+    result = first_number + second_number
+    #Imprime result (suma de los dos números)
+    print(result)
+
+
+#Añade convención
+if __name__ == "__main__":
+    sum_two_nums()
